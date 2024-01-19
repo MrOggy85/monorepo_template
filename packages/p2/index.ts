c32b7630030ab5293e7eecfc3c37725319bd06e3
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2024-01-19 15:58:25 +0900

    init

---
+++ packages/p2/index.ts
@@ -0,0 +1,3 @@
+import '../shared/shared'
+
+console.log('p2')
---
+++ packages/shared/shared.ts
@@ -0,0 +1,8 @@
+const hej: number = 1
+
+enum FRUIT {
+  BANANA = 'banana',
+  TOMATO = 'tomato'
+}
+
+console.log('shared', FRUIT.BANANA)
